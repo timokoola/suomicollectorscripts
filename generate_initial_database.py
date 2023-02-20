c685e1eb2c90c1ec84dc24a49952298a66873eb4
Timo Koola <timo.koola@gmail.com>
2023-02-20 20:41:44 +0200

    new initial database script, now handles every file one by one

--- generate_initial_database.py
+++ generate_initial_database.py
@@ -1,9 +1,10 @@
 import json
-from typing import List
+from typing import Dict, List
 import requests
 from bs4 import BeautifulSoup
 import os.path
 import jsonlines
+import libvoikko
 
 GUTENBERG_fi = "https://www.gutenberg.org/browse/languages/fi"
 
@@ -89,6 +90,60 @@ def get_key(word: dict) -> str:
     return f"{word['BOOKWORD']}_{word['av']}_{word['tn']}_{word['SIJAMUOTO']}_{word['NUMBER']}"
 
 
+def get_book_text(book: str) -> str:
+    f = open(f"gutenberg/{book}")
+    book_text = f.read()
+    f.close()
+    book_text = " ".join(book_text.lower().split())
+    return book_text
+
+
+def get_voikko_for_book(book_text: str):
+    v = libvoikko.Voikko("fi")
+    word_forms = [
+        (word, v.analyze(word))
+        for word in book_text.split()
+        if len(v.analyze(word)) > 0
+    ]
+
+    return word_forms
+
+
+def flatten_voikko_results(word_forms):
+    flat_words = []
+    for item in word_forms:
+        word = item[0]
+        for i in item[1]:
+            flat_words.append({"BOOKWORD": word.lower(), **i})
+    return flat_words
+
+
+def get_book_words_in_kotus(kotus_dict, flat_words):
+    gutenberg_results = []
+    for bw in flat_words:
+        baseform = bw["BASEFORM"]
+        if baseform in kotus_dict and kotus_dict[baseform]["tn"] < 53:
+            gutenberg_results.append({**kotus_dict[baseform], **bw})
+    return gutenberg_results
+
+
+def clear_console_line():
+    print("\033[K", end="")
+
+
+def extract_unique_words(unique_words, gutenberg_results):
+    unique_gutenberg_words = []
+    for item in gutenberg_results:
+        # if the word is not in the unique words set
+        # add it to the results list
+        # and add it to the unique words set
+        key = item["BOOKWORD"]
+        if key not in unique_words:
+            unique_gutenberg_words.append(item)
+            unique_words.add(key)
+    return unique_gutenberg_words
+
+
 if __name__ == "__main__":
     links = get_links(GUTENBERG_fi)
     for link in links:
@@ -96,61 +151,55 @@ if __name__ == "__main__":
 
     # generate the initial result list
     nimentos = get_kotus_nimentos()
-    results: dict = dict([(get_key(nimento), nimento) for nimento in nimentos])
+    results = nimentos
 
     print(f"Found {len(results)} nimentos")
-
-    # loop through all books in gutenberg directory
-    full_text = []
-    for book in os.listdir("gutenberg"):
-        if book.endswith(".txt"):
-            print(f"Processing {book}")
-            f = open(f"gutenberg/{book}")
-            book_text = f.read()
-            f.close()
-            book_text = " ".join(book_text.lower().split())
-            full_text.append(book_text)
-
-    fullest_text = " ".join(full_text)
-    normalized_full = list(set(fullest_text.split()))
-
-    # write full text file for posterity
-    with open(fullest_text_file, "w+") as f:
-        f.write(fullest_text)
-
-    # run all words through voikko
-    v = libvoikko.Voikko("fi")
-    word_forms = [
-        (word, v.analyze(word)) for word in normalized_full if len(v.analyze(word)) > 0
-    ]
-
-    kotus = get_kotus_data()
-    flat_words = []
-    for item in word_forms:
-        word = item[0]
-        for i in item[1]:
-            flat_words.append({"BOOKWORD": word.lower(), **i})
-
-    book_bw = set([w["BASEFORM"] for w in flat_words])
-    kotus_w = set([w["word"] for w in kotus])
-
-    kotus_dict = dict([(x["word"], x) for x in kotus])
-
-    # words from gutenberg that have been run through voikko
-    gutenberg_results = []
-    for bw in flat_words:
-        baseform = bw["BASEFORM"]
-        if baseform in kotus_dict:
-            gutenberg_results.append({**kotus_dict[baseform], **bw})
-
-    # merge results from kotus and gutenberg
-    for result in gutenberg_results:
-        key = get_key(result)
-        if key in results:
-            results[key]["COUNT"] += 1
-        else:
-            results[key] = result
-            results[key]["COUNT"] = 1
-
+    # write the initial result list to file
     with jsonlines.open("output.jsonl", "w") as writer:
         writer.write_all(results)
+
+    # get kotus data
+    kotus = get_kotus_data()
+    # set to determine if we need to process the word
+    unique_words = set([w["word"] for w in kotus])
+    # kotus words as a dictionary for faster lookup
+    kotus_dict = dict([(x["word"], x) for x in kotus])
+
+    # count txt files in gutenberg directory
+    txt_files = len([f for f in os.listdir("gutenberg") if f.endswith(".txt")])
+    file_index = 1
+
+    # loop through all books in gutenberg directory
+    for book in os.listdir("gutenberg"):
+        if book.endswith(".txt"):
+            clear_console_line()
+            print(f"Processing {book} ({file_index} of {txt_files})", end=" ")
+            book_text = get_book_text(book)
+            # run all words through voikko
+            # note same word can produce multiple forms
+            word_forms = get_voikko_for_book(book_text)
+
+            flat_words = flatten_voikko_results(word_forms)
+
+            # words from current book that have been run through voikko
+            # as a set for faster lookup
+            book_bw = set([w["BASEFORM"] for w in flat_words])
+
+            # words from gutenberg that have been run through voikko
+            gutenberg_results = get_book_words_in_kotus(kotus_dict, flat_words)
+
+            unique_gutenberg_words = extract_unique_words(
+                unique_words, gutenberg_results
+            )
+
+            # report added unique words
+            print(
+                f"Added {len(unique_gutenberg_words)} unique words from {book}",
+                end="\r",
+            )
+
+            # append gutenbergs results to the output file
+            with jsonlines.open("output.jsonl", "a") as writer:
+                writer.write_all(unique_gutenberg_words)
+
+            file_index += 1
